e11943d12cfd7a00b63cbb83860b1a7b96d8ab49
JhonJimenez <jhon.jimenez@siemens-energy.com>
2025-03-12 07:08:44 -0500

    Ultimo reporte de cambios

--- hellogit.py
+++ hellogit.py
@@ -1,2 +1,6 @@
-print ("Hello git 2")
-print ("Hola mundo 2")
+# Definir una lista de variables
+variables = ["variable1", "variable2", "variable3"]
+
+# Imprimir el nombre de cada variable en la lista
+for nombre in variables:
+    print(f"El nombre de la variable es: {nombre}")
--- hellogit2.py
+++ hellogit2.py
@@ -1,2 +1,35 @@
-print ("Hola mundo git 2 vr master")
-print ("Hola mundo 2")
+print ("Juego de la raza malvada contra la benevola")
+print ("Ingresa la raza malvada")
+malvada = ["lolos","fulanos","hoggins","lurcos","trollis"]
+cantMalos = {}
+for malos in malvada:
+    cantMalos[malos] = input(f"Introduce una cantidad de soldados {malos}: ")
+
+print ("Ingresa la raza benevola")
+benevola = ["ositos","principes","enanos","caris","fulos"]
+cantBuenos = {}
+for buenos in benevola:
+    cantBuenos[buenos] = input(f"Introduce una cantidad de soldados {buenos}: ")
+
+totalMalvada = 0
+suma = 1
+for poderM in cantMalos:
+    totalMalvada += int(cantMalos[poderM]) * suma
+    suma += 1
+
+totalBuenos = 0
+suma = 1
+for poderB in cantBuenos:
+    totalBuenos += int(cantBuenos[poderB]) * suma
+    suma += 1
+
+print (f"El total de poder de soldados malos es de: {totalMalvada}")
+print (f"El total de poder de soldados buenos es de: {totalBuenos}")
+
+print ("El resultado es:")
+if totalMalvada > totalBuenos:
+    print("El resultado es: gana el mal")
+elif totalBuenos > totalMalvada:
+    print("El resultado es: gana el bien")
+else:
+    print("El resultado es: empate")
