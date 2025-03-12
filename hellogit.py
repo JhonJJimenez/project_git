48be35929f28a570cf06f15e574b3e9ded51cd4b
JhonJimenez <jhon.jimenez@siemens-energy.com>
2025-03-12 07:54:03 -0500

    Hellogit v3

--- hellogit.py
+++ hellogit.py
@@ -4,3 +4,5 @@ variables = ["variable1", "variable2", "variable3"]
 # Imprimir el nombre de cada variable en la lista
 for nombre in variables:
     print(f"El nombre de la variable es: {nombre}")
+
+print ("Hello git v2")
